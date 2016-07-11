0e010ffce7164647a91a7cbd1ca931333d3efe7d
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-07-11 13:54:50 -0600

    add all passing tests and finished server code

--- test/server-test.js
+++ test/server-test.js
@@ -59,8 +59,8 @@ describe('Server', () => {
         if (error) { done(error);}
         var pizzaCount = Object.keys(app.locals.pizzas).length;
         assert.equal(pizzaCount, 1, `expect 1 pizzas, found ${pizzaCount}`)
+        done();
       });
-      done();
     });
 
     it('should not return 404', (done) => {
@@ -70,6 +70,65 @@ describe('Server', () => {
         done();
       });
     });
+
+    it('should redirect the user to their new pizza', (done) => {
+      var payload = { pizza: fixtures.validPizza };
+
+      this.request.post('/pizzas', { form: payload }, (error, response) => {
+        if (error) { done(error); }
+        var newPizzaId = Object.keys(app.locals.pizzas)[0];
+        assert.equal(response.headers.location, '/pizzas/' + newPizzaId);
+        done();
+      });
+    });
+
   });
+
+  describe('GET /pizzas/:id', () => {
+
+    beforeEach(() => {
+      app.locals.pizzas.testPizza = fixtures.validPizza;
+    });
+
+    it('should not return 404', (done) => {
+      this.request.get('/pizzas/testPizza', (error, response) => {
+      if (error) { done(error); }
+      assert.notEqual(response.statusCode, 404);
+      done();
+    });
+  });
+
+  it('should return a page that has the title of the pizza', (done) => {
+    var pizza = app.locals.pizzas.testPizza;
+
+    this.request.get('/pizzas/testPizza', (error, response) => {
+      if (error) { done(error); }
+      assert(response.body.includes(pizza.name),
+           `"${response.body}" does not include "${pizza.name}".`);
+      done();
+    });
+  });
+
+  it('should return all the topings in a list', (done) => {
+    var pizza = app.locals.pizzas.testPizza;
+
+    this.request.get('/pizzas/testPizza', (error, response) => {
+      if (error) {done(error);}
+
+      pizza.toppings.forEach(function(topping) {
+      assert(response.body.includes(pizza.toppings[0]),
+              `${response.body} does not include ${pizza.toppings}`);
+        });
+      done();
+    });
+  });
+
+
+
+
+
+
+
+});
 
 });
