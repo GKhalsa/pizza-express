79772729e88a1fb515e047f86df5b7626bf66cd4
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-07-11 11:23:38 -0600

    add views dir

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app     = express();
+const path    = require('path');
+
+app.use(express.static('static'));
+app.set('view engine', 'jade');
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Pizza Express';
 
 app.get('/', (request, response) => {
-  response.send(app.locals.title);
+  response.sendFile(path.join(__dirname, '/static/index.html'));
 });
 
 app.listen(app.get('port'), () => {
